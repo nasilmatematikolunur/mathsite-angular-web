424a266a869e2dccd71cf56b91dc29800316607d
Semih Şen <halil.semih.sen@gmail.com>
2022-04-30 00:58:40 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NaviComponent } from './navi/navi.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NaviComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
